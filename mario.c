c716e81b2c0da24edcf7f48c4f9eb73044e39685
SKovaS <SKovaS@users.noreply.github.com>
2016-10-02 20:57:44 +0200

    Update mario.c

--- mario.c
+++ mario.c
@@ -4,36 +4,35 @@
 
 
 
-int main(){
-
+int main()
+{
         int raz;
         int size=23;
-        while(true){
+        while(true)
+        {
             printf("height : ");
-       raz=get_int();
-        if(raz<=size&&raz>=0){
-            break;}
+            raz=get_int();
+            if(raz<=size&&raz>=0)
+            {
+            break;
+            }
         }
         int q;
         int j = 0;
         int i = 0;
         for(i=0;i<raz;i++)
         {
-        for(q=raz;q>i+1;q--)
-        {
-        printf(" ");
+            for(q=raz;q>i+1;q--)
+            {
+            printf(" ");
+            }
+              for(j=0;j <i+2;j++)
+              {
+              printf("#");
+              }
+              printf("\n");
         }
-        for(j=0;j <i+2;j++){
-        printf("#");
-        }
-        printf("\n");
-
-
-        }
-
-
-     return 0;
-
-      }
+        return 0;
+}
 
 
