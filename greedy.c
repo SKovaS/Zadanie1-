faf154a756952c54d591c9943ead0505958c2fac
SKovaS <SKovaS@users.noreply.github.com>
2016-10-02 21:00:58 +0200

    Update greedy.c

--- greedy.c
+++ greedy.c
@@ -8,18 +8,22 @@ int main()
     int mas[]={25,10,5,1};
     float n=-1;
     int count=0;
-    while(n<0){
-        printf("O hai! How much change is owed?\n");
-    n=get_float();
-    n=n*100;
-    n=round(n);
-    }
+      while(n<0)
+      {
+      printf("O hai! How much change is owed?\n");
+      n=get_float();
+      n=n*100;
+      n=round(n);
+      }
     int i=0;
-    for(i=0;i<4;i++){
-    while(n>=mas[i]){
-    n=n-mas[i];
-        count++;}
-    }
+       for(i=0;i<4;i++)
+       {
+         while(n>=mas[i])
+         {
+         n=n-mas[i];
+         count++;
+         }
+       }
     printf("%d\n",count);
-    }
+}
 
