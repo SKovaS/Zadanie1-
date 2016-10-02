6ab30d9c0fe0257e9334b5291f217745589e3eb0
SKovaS <SKovaS@users.noreply.github.com>
2016-10-02 19:59:52 +0200

    Update greedy.c

--- greedy.c
+++ greedy.c
@@ -1,27 +1,25 @@
-#include<cs50.h> 
-#include<stdio.h> 
-#include <math.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <cs50.h>
 
-int main (void) 
-{ 
-float n = -1; 
-printf("O hai! "); 
-while (n < 0) 
-{ 
-printf("How much change is owed?\n"); 
-n = GetFloat()*100; 
-n = round(n);
-}; 
-int count = 0; 
-int A[] = {25, 10, 5, 1}; 
-int i=0; 
-for (i = 0; i < 4; i++) 
-{ 
-while (n >= A[i]) 
-{ 
-n = n - A[i]; 
-count++; 
-} 
-} 
-printf("%d\n", count); 
-}
+int main()
+{
+    int mas[]={25,10,5,1};
+    float n=-1;
+    int count=0;
+    while(n<0){
+        printf("O hai! How much change is owed?\n");
+    n=get_float();
+    n=n*100;
+    n=round(n);
+    }
+    int i=0;
+    for(i=0;i<4;i++){
+    while(n>=mas[i]){
+    n=n-mas[i];
+        count++;}
+    }
+    printf("%d\n",count);
+    }
+
